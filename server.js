e694918229dff147142fb7f7dac41fa3b5bfc263
Shaggy2606 <shagun.khemka60@gmail.com>
2017-04-14 22:04:34 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Shagun on 14/04/2017.
+ */
+//get environment variables
+require('dotenv').config();
+
+var express=require('express');
+var app=express();
+var LanguageTranslatorV2 = require('watson-developer-cloud/language-translator/v2');
+var language_translator = new LanguageTranslatorV2({
+    username: "e0568806-5154-4edb-9574-0a3094ce9fb7",
+    password: "1SzwUQsLxntO",
+    url: 'https://gateway.watsonplatform.net/language-translator/api/'
+});
+
+app.get('/api',function (req,res) {
+
+    var lang=req.param('word');
+    language_translator.translate({
+            text: lang, source : 'en', target: 'es' },
+        function (err, translation) {
+            if (err)
+                console.log('error:', err);
+            else
+                res.send({Spanish : translation});
+        });
+
+
+
+});
+
+var server = app.listen(process.env.PORT || 7000, function(){
+    console.log('Server listening on port 7000');
+});
